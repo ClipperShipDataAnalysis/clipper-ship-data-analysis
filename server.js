2b31a4e9ae9f63aed681dff3bf8281fbbc1a2639
John Lervandal <55460368+JohnLervandal@users.noreply.github.com>
2023-02-04 10:33:05 -0500

    can now access database from html pages

--- server.js
+++ server.js
@@ -7,9 +7,23 @@ app.set('view engine', 'ejs');
 
 mongoose.connect("mongodb+srv://john:Jn4wXiyqcn6u1vGo@cluster0.lcwl3.mongodb.net/test")
 
+const moviesSchema = {
+    index: Array,
+    columns: Array,
+    data: Array,
+    index_names: Array,
+    column_names: Array
+
+}
+
+const Movie = mongoose.model("Movie", moviesSchema);
+
 app.get('/', (req, res) => {
-    let name = "John";
-    res.render('index', {userName: name});
+    Movie.find({}, function(err, movies) {
+        res.render('index', {
+            moviesList: movies
+        })
+    })
 })
 
 app.listen(4000, function(){
