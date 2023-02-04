218c72cb86a0e61e85a8c1d15e2f609f92dd50c0
mcap <64444406+Mcaplovitz@users.noreply.github.com>
2023-02-04 15:03:45 -0500

    Got css and pic working by adding filepath to server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const ejs = require('ejs');
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/clipper-ship-data-analysis'));
+app.use(express.static(__dirname + '/frontend'));
 
 mongoose.connect("mongodb+srv://john:Jn4wXiyqcn6u1vGo@cluster0.lcwl3.mongodb.net/ClipperShipGrantData?retryWrites=true&w=majority")
 
@@ -14,23 +14,23 @@ const collection = db.collection('ClipperShipGrantData');
 console.log(collection);
 
 const moviesSchema = new mongoose.Schema({
-	index: Array,
-	columns: Array,
-	data: Array,
-	index_names: Array,
-	column_names: Array
+    index: Array,
+    columns: Array,
+    data: Array,
+    index_names: Array,
+    column_names: Array
 });
 
 
 const Movie = mongoose.model("Movie", moviesSchema);
 
 app.get('/', (req, res) => {
-	collection.findOne({}, function(err, doc) {
-		if (err) throw err;
-		res.render('index', {
-			moviesList: doc
-		})
-	});
+    collection.findOne({}, function(err, doc) {
+        if (err) throw err;
+        res.render('index', {
+            moviesList: doc
+        })
+    });
 })
 
 app.listen(4000, function() {
