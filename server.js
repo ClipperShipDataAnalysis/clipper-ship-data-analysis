7d09cad84cffede5e2854ff954518afe4d258adf
fritztheritz <fduverglas@brandeis.edu>
2023-02-04 15:14:12 -0500

    got redirect working

--- server.js
+++ server.js
@@ -33,6 +33,11 @@ app.get('/', (req, res) => {
     });
 })
 
+app.get('/data', (req, res) => {
+	res.render("data");
+}
+)
+
 app.listen(4000, function() {
     console.log('Server is running on port 4000');
 })
